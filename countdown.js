1bc53da7aec44547f10b196f28ffee5fc16a86b7
Albie-feary <laurabeth.feary@gmail.com>
2017-04-14 16:39:07 +1200

    add startCountdown function and global variables

--- countdown.js
+++ countdown.js
@@ -1,3 +1,26 @@
+// global variables (so used in different functions)
+var secondsRemaining;
+var intervalHandle;
+
+
+
+function startCountdown(){
+  // get the contents of the "minutes" text box which we are using the .value propert of.
+  var minutes = document.getElementbyId("minutes").value;
+  // check if it is not a number using isNaN
+if (isNaN(minutes)){
+  alert("Please enter a number!");
+  return;
+  // return just means im done with this function, I cannot go any further so pop out now
+}
+// however if it is a number that has been entered then move past the if statement and find out how many seconds are remaining.
+// how many seconds?
+secondsRemaining = minutes * 60;
+// every second(1000milliseconds), call the tick function
+intervalHandle = setInterval(tick, 1000);
+// hide the form when the clock is ticking
+document.getElementbyId("inputArea").style.display="none";
+};
 
 
 
