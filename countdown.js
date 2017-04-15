1ee15f871c643448b4f73904128ca1341a62c235
Albie-feary <laurabeth.feary@gmail.com>
2017-04-15 12:11:55 +1200

    add reset page function

--- countdown.js
+++ countdown.js
@@ -2,23 +2,36 @@
 var secondsRemaining;
 var intervalHandle;
 
+// reset page function to reset the countdown again.
+function resetPage() {
+  document.getElementbyId("inputArea").stlye.display ="block";
+}
+
 function tick(){
   // grab hold of the h1 by creating a var called timeDisplay
   var timeDisplay = document.getElementbyId("time");
   // then turn the seconds that are in the secondsRemaining var into minutes and seconds.
   // math.floor makes a number a basic integer, just ignore any remainder.
   var min = Math.floor(secondsRemaining / 60);
-  var sec = secondsRemaining - (* 60);
+  var sec = secondsRemaining - (min * 60);
   // if seconds are less than 10, then add a leading zero as a string value
   if (sec<10) {
     sec = "0" + sec;
-    // take min and seconds value and concatenate using colon
+  }
+  // take min and seconds value and concatenate using colon
     var message = min + ":" + sec;
     // and finally we change what the page actually displays
     timeDisplay.innerHTML = message;
+    // Did the seconds make it down to zero?
+    if (secondsRemaining === 0) {
+      alert("Done!");
+      clearInterval(intervalHandle);
+      resetpage();
 
-  }
-};
+    }
+    // subtract one from the seconds remaining, because this is the function thats called every second.
+    secondsRemaining --;
+}
 
 
 function startCountdown(){
@@ -43,22 +56,22 @@ document.getElementbyId("inputArea").style.display="none";
 
 
 // as soon as page loads,load this code and what we are doing here is creating some new elements
-window.onload =function() {
-  // create an input text box and give it an id of "minutes"
-  var inputMinutes = document.createElement("input");
-  inputMinutes.createAttribute(id, "minutes");
-  inputMinutes.createAttribute(type, "text");
+window.onload = function() {
+  // create an input text box and give it an id of "minutes
+    var inputMinutes = document.createElement("input");
+  inputMinutes.setAttribute("id", "minutes");
+  inputMinutes.setAttribute("type", "text");
 
   // create the button
   var startButton = document.createElement("input");
-  startButton.createAttribute(type, "button");
-  startButton.createAttribute(value, "Start Countdown");
+  startButton.setAttribute("type", "button");
+  startButton.setAttribute("value", "Start Countdown");
 
   // associate that click event of the button with a function called Start Countdown
-startButton.onclick = function(){
+startButton.onclick = function() {
   startCountdown();
 };
 // Take the two new elements and add them to the div called "inputArea"
-document.getElementbyId("inputArea").appendChild(inputMinutes);
-document.getElementbyId("inputArea").appendChild(startButton);
+document.getElementById("inputArea").appendChild(inputMinutes);
+document.getElementById("inputArea").appendChild(startButton);
 };
